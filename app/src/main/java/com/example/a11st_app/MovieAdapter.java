f0a684478e5b567ccb202e9a3b791485a98724b3
COMPUTER <duwlsrla0101@gmail.com>
2021-12-01 03:16:00 +0900

    2021_11_24 Movie,Movielist,MovieListResult 추가

---
+++ app/src/main/java/com/example/a11st_app/MovieAdapter.java
@@ -0,0 +1,63 @@
+package com.example.a11st_app;
+
+import android.graphics.Movie;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+    ArrayList<android.graphics.Movie> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        View itemView = inflater.inflate(R.layout.movie_item, viewGroup, false);
+
+        return new ViewHolder(itemView);
+    }
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position){
+        Movie item = items.get(position);
+        viewHolder.setItem(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    public void addItem(Movie item){
+        items.add(item);
+    }
+
+    public android.graphics.Movie getItems(int position) {
+        return items.get(position);
+    }
+
+    public void setItems(ArrayList<android.graphics.Movie> items) {
+        this.items = items;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        TextView textView2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textView = itemView.findViewById(R.id.textView);
+            textView2 = itemView.findViewById(R.id.textView2);
+        }
+
+        public void setItem(Movie item){
+            textView.setText(item.movieNm);
+            textView.setText(item.audiCnt + "명");
+        }
+    }
+}
+
